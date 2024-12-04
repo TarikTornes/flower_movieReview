fedc71ab69b90a253610f00db7a8efbb69669868
TarikTornes <tarik.tornes03@gmail.com>
2024-12-04 23:06:06 +0100

    main: Added the new features to the pipeline

--- src/main.py
+++ src/main.py
@@ -1,12 +1,26 @@
 from .utils.reviewloader import ReviewLoader
+from .preprocessing.dataset import prepare_dataset
+from .client.client import generate_client_fn
+
+import hydra
+from omegaconf import DictConfig, OmegaConf
+import flwr
 import os
 
+@hydra.main(config_path="../conf", config_name="base", version_base=None)
+def main(cfg: DictConfig):
 
-def main():
-    print(os.getcwd())
-    rl = ReviewLoader("./data/train/")
+    print(OmegaConf.to_yaml(cfg))
 
-    print(rl.get_df().head)
+    data = None
+
+    # Prepare our dataset
+    trainloaders, validationloaders, testloader = prepare_dataset(data, cfg.num_clients, cfg.batch_size)
+
+    # Define clients
+    client_fn = generate_client_fn(trainloaders, validationloaders, cfg.num_classes)
+
+
 
 
 if __name__=="__main__":

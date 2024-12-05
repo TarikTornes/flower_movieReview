d268af6dd6f71e8cf962708fdfc90c22c71ee78f
TarikTornes <tarik.tornes03@gmail.com>
2024-12-05 10:56:58 +0100

    Added the server component and strategy

--- src/main.py
+++ src/main.py
@@ -1,10 +1,11 @@
 from .utils.reviewloader import ReviewLoader
 from .preprocessing.dataset import prepare_dataset
 from .client.client import generate_client_fn
+from .server.server import get_on_fit_config, get_eval_fn
 
 import hydra
 from omegaconf import DictConfig, OmegaConf
-import flwr
+import flwr as fl
 import os
 
 @hydra.main(config_path="../conf", config_name="base", version_base=None)
@@ -19,6 +20,22 @@ def main(cfg: DictConfig):
 
     # Define clients
     client_fn = generate_client_fn(trainloaders, validationloaders, cfg.num_classes)
+
+
+    # Define the strategy to aggregate the weights
+    # fraction_fit will sample a certain fraction of clients for a round
+    # by default it is 1.0, we set it very small in order to use min_fit_clients parameter
+    # since min_fit_clients will be taken if it is larger than fraction_fit * num_clients
+    # therefore we set it very small
+    strategy = fl.server.strategy.FedAvg(fraction_fit=0.000000001,
+                                         min_fit_clients=cfg.num_clients_per_round_fit,
+                                         fraction_evaluate=0.0000001,
+                                         min_evaluate_clients=cfg.num_clients_per_round_eval,
+                                         min_available_clients=cfg.num_clients,
+                                         on_fit_config_fn=get_on_fit_config(cfg.config_fit),
+                                         evaluate_fn=get_eval_fn(cfg.num_classes, testloader))
+
+    # Start Simulation
 
 
 
---
+++ src/server/server.py
@@ -0,0 +1,61 @@
+from omegaconf import DictConfig
+from ..model.model import Net, test
+
+from collections import OrderedDict
+
+import torch
+
+def get_on_fit_config(config: DictConfig):
+    ''' This function returns a function allows us to configure/adapt 
+        certain hyperparameters dependign on the round we are for 
+        example increase or decrease the learning rate, etc.
+
+        Args:
+            server_round: the round the client is training in
+
+        Return:
+            fit_config_fn: Function that contains the constraints depending on the round
+
+    '''
+
+    def fit_config_fn(server_round: int):
+        
+        # DEFINE SOME CONDITIONS IF NEEDED
+
+        return {'lr': config.lr, 'momentum': config.momentum, 
+                'local_epochs': config.local_epochs}
+
+    return fit_config_fn
+
+
+def get_eval_fn(num_classes, testloader):
+
+    def eval_fn(server_round: int, parameters, config):
+
+        model = Net(num_classes)
+
+        if torch.cuda.is_available():
+            device = "cuda"
+        elif torch.mps.is_available():
+            device = "mps"
+        else:
+            device = "cpu"
+
+        device = torch.device(device)
+
+
+        # NEEDS TO BE MODIFIED DEPENDING ON MODEL
+        # Supposed to set the new parameters for the central model
+        # for evaluation
+        params_dict = zip(model.state_dict().keys(), parameters)
+        state_dict = OrderedDict({k: torch.Tensor(v) for k, v in params_dict})
+        model.load_state_dict(state_dict, strict=True)
+
+        loss, acc = test(model, testloader, device)
+
+        return loss, {'loss': loss, 'accuracy': acc}
+
+        
+
+    return eval_fn
+
